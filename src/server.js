0a64f922b26df72a673e9a042180701e29a54e44
Emily Kaneff <emilykaneff@Emilys-MacBook-Pro.local>
2017-03-16 02:19:42 -0400

    added /v1 to route

--- src/server.js
+++ src/server.js
@@ -6,8 +6,8 @@ const app = express();
 app.use(bodyParser.json());
 
 // Routes
-app.use('/', require('./routes/v1.0.0/user.js')(express));
-app.use('/', require('./routes/v1.0.0/character.js')(express));
+app.use('/v1', require('./routes/v1.0.0/user.js')(express));
+app.use('/v1', require('./routes/v1.0.0/character.js')(express));
 
 // Server Active
 module.exports = app.listen(port, () => {
