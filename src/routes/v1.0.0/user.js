424c345e172c732621f389287261785449a614fd
Emily Kaneff <emilykaneff@Emilys-MacBook-Pro.local>
2017-03-16 19:06:30 -0400

    updated get route for user

--- src/routes/v1.0.0/user.js
+++ src/routes/v1.0.0/user.js
@@ -1,12 +1,12 @@
 module.exports = (express) => {
 	const router = express.Router();
 
-	const Sequelize = require('sequelize');
-	const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD);
+	//const Sequelize = require('sequelize');
+	//const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD);
 
 	//models
-	//const User = require('../models').User;
-	///const MatchData = require('../models').MatchData;
+	const User = require('../models').User;
+	const MatchData = require('../models').MatchData;
 
 	router.get('/user/test', (req, res) => {
 		res.json({healthy: true});
@@ -23,6 +23,42 @@ module.exports = (express) => {
 						//foreach through array
 							//temp += arrayvariable
 						//res.json{uid, healing, damage, wins, losses}
+		MatchData.findAll({
+			where: {
+				userId: req.params.uid
+			}
+		}).then(function(matches) {
+			var tempHealing = 0;
+			var tempDamage = 0;
+			var tempWins = 0;
+			var tempLoss = 0;
+
+			matches.forEach((match) => {
+				tempHealing += match.healing;
+				tempDamage += match.damage;
+				if (match.result == 1) {
+					tempWins += 1;
+				}
+				if (match.result == 0) {
+					tempLoss += 1;
+				} 
+			});
+
+			User.findAll({
+				where: {
+					userId: req.params.uid
+				}
+			}).then(function(user) {
+				res.json({
+					id: user.id,
+					username: user.username,
+					healing: tempHealing,
+					damage: tempDamage,
+					wins: tempWins,
+					losses: tempLoss
+				});
+			});
+		});
 	});
 
 	// add new user to the database
@@ -36,3 +72,7 @@ module.exports = (express) => {
 
 	return router;
 }
+
+
+
+
