0c3f7300d43ee59caeea6dec91c56ea9922219ea
Emily Kaneff <emilykaneff@Emilys-MacBook-Pro.local>
2017-03-16 00:16:18 -0400

    added skeleton of user route with sudo code

---
+++ src/routes/user.js
@@ -0,0 +1,28 @@
+module.exports = (express) => {
+	const router = express.Router();
+
+	const Sequelize = require('sequelize');
+	const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD);
+
+	//models
+	const User = require('../models').User;
+	const MatchData = require('../models').MatchData;
+
+	// get users based on ID, will return all stats for that character
+	router.get('/user/:uid', (req, res) => {
+		//get user based on id
+			//.then
+				//get matches based on user id
+					//.then
+						//assign results to array var
+						//create temp healing, damage, wins and losses variable
+						//foreach through array
+							//temp += arrayvariable
+						//res.json{uid, healing, damage, wins, losses}
+	});
+
+	// add new user to the database
+	router.post('/user/:uname', (req, res) => {
+
+	});
+}
