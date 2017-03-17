0084c41a9b4bc83103d1a1e400e4165c6d5505b7
Bailey Charlton <bbcharlton@fullsail.edu>
2017-03-16 20:23:38 -0400

    Updated character route to have several checks. Still needs models

--- src/routes/v1.0.0/character.js
+++ src/routes/v1.0.0/character.js
@@ -3,7 +3,6 @@ module.exports = (express) => {
 
 	// Sequelize Database Connection
 	const Sequelize = require('sequelize');
-    const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD);
 
     // Models
     // UNCOMMENT THE BELOW CODE AND DELETE THIS LINE WHEN MODELS HAVE BEEN CREATED
@@ -18,29 +17,54 @@ module.exports = (express) => {
 
     // Get all stats for a specified user's character
 	router.get('/character/:uid/:cid', (req, res) => {
-		MatchData.findAll({
+		// Check if user exists
+		User.findOne({
 			where: {
-				user_id: req.params.uid,
-				char_id: req.params.cid
+				user_id: req.params.uid
 			}
-		}).then(function (result) {
-			var tempDamage = 0;
-			var tempHealing = 0;
-			var tempWins = 0;
-			var tempLosses = 0;
+		}).then((user) => {
+			// Check if character exists
+			Character.findOne({
+				where: {
+					char_id: req.params.cid
+				}
+			}).then((character) => {
+				// Check if match data exists
+				MatchData.findAll({
+					where: {
+						player_id: req.params.uid,
+						character_id: req.params.cid
+					}
+				}).then(function(result) {
+					var tempDamage = 0;
+					var tempHealing = 0;
+					var tempWins = 0;
+					var tempLosses = 0;
 
-			tempDamage += result.damage;
-			tempHealing += result.healing;
+					// Loop through all match data results
+					result.forEach((match) => {
+						tempDamage += match.damage;
+						tempHealing += match.healing;
 
-			if (result.result == 0) {
-				tempLosses += 1;
-			} else {
-				tempWins += 1;
-			}
+						// Check if match is a win (1) or a loss (0)
+						if (match.result == 0) {
+							tempLosses += 1;
+						} else {
+							tempWins += 1;
+						}
 
-			// Returns the specified user's character's total stats
-			res.json({userID: req.params.uid, charID: req.params.cid, totalDamage: tempDamage, totalHealing: tempHealing, totalWins: tempWins, totalLosses: tempLosses});
-		});
+						// Returns the specified user's character's total stats
+						res.json({userID: req.params.uid, userName: user.username, charID: req.params.cid, charName: character.name, totalDamage: tempDamage, totalHealing: tempHealing, totalWins: tempWins, totalLosses: tempLosses});
+					});
+				}).catch((err) => {
+					res.json({error: err});
+				}); // End of match data exists check
+			}).catch((err) => {
+				res.json({error: err});
+			}); // End of character exists check
+		}).catch((err) => {
+			res.json({error: err});
+		}); // End of user exists check
 	});
 
 	return router;
